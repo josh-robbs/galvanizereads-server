d8f60d659baef5cbe2bbad429ff12803224d351a
Josh Robbs <joshrobbs@Zuul.local>
2018-08-27 13:59:18 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const port = parseInt(process.env.PORT || 8080)
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+app.get('/', (req,res,next) => {  
+  res.json({
+    message: 'Hello World!'
+  }); 
+})
+
+
+app.listen(port, () => {console.log(`Tacos available on port ${port}`)})
