8b20fb366d9a24c8f84cee6c4d1bdc130657d0fa
Josh Robbs <joshrobbs@Zuul.local>
2018-08-28 15:39:54 -0600

    changing queries to figure out the author list on the book list component

--- database/queries.js
+++ database/queries.js
@@ -6,8 +6,8 @@ module.exports = {
     return database('book')
       .select('book.cover_url', 'book.title', 'author.first_name', 'author.last_name', 'book.genre', 'book.description')
       .from('book')
-      .innerJoin('author_book', 'book.id', 'author_book.book_id') 
-      .innerJoin('author', 'author.id', 'author_book.author_id') 
+      .innerJoin('author_book', 'book.id', 'author_book.book_id')
+      .innerJoin('author', 'author.id', 'author_book.author_id')
   },
   listAuthors(){
     return database('author').select('author.pic_url', 'author.first_name', 'author.last_name', 'author.bio', 'book.title')
@@ -15,7 +15,18 @@ module.exports = {
     .innerJoin('author_book', 'author.id', 'author_book.author_id') 
     .innerJoin('book', 'book.id', 'author_book.book_id') 
   },
+  listAuthorsById(id){
+    return database('author').select('author.pic_url', 'author.first_name', 'author.last_name', 'author.bio', 'book.title')
+    .from('author')
+    .innerJoin('author_book', 'author.id', 'author_book.author_id') 
+    .innerJoin('book', 'book.id', 'author_book.book_id') 
+    .where('book.id',id)
+  },
+  list(tableName){
+    return database(tableName).select()
+  },
   read(id,tableName){
+    // send in column name to query for anything
     return database(tableName).select().where('id', id)
   },
   post(product,tableName){
--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const queries = require('../database/queries')
 
 router.get('/',(req,res,next) => {
-  queries.listBooks()
+  queries.list('book')
     .then(book => {
       res.json({book})
     })
